adaacd98856d87b0cec39add2b9082edc3db0465
Kento Yoshikawa <y1510161@edu.cc.uec.ac.jp>
2018-02-09 17:26:48 +0900

    added reranked images

--- Repo2/code/report2_prep.m
+++ Repo2/code/report2_prep.m
@@ -3,12 +3,26 @@
 %web画像の読み込み Flickerにcomputerと検索したときの画像300枚
 textlist=textread('urllist.txt','%s');
 
-%画像のダウンロード
-{
+%web画像の読み込み Flickerにfried eggと検索したときの画像300枚
+textlist_another=textread('friedegglist.txt','%s');
+
+%コンピュータの画像のダウンロード
+%{
 OUTDIR='img';
 mkdir(OUTDIR);
 for i=1:size(textlist,1)
   fname=strcat(OUTDIR,'/',num2str(i,'%04d'),'.jpg');
-  websave(fname,list{i});
+  websave(fname,textlist{i});
 end
-}
+%}
+
+%目玉焼きの画像のダウンロード
+
+  OUTDIR='fried_egg';
+  mkdir(OUTDIR);
+  for i=1:size(textlist_another,1)
+	  fname=strcat(OUTDIR,'/',num2str(i,'%04d'),'.jpg');
+  websave(fname,textlist_another{i});
+end
+ 
+
